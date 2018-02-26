a9a5c1f39b8cb4c48a0c282fbfffecdcbd2487ab
vento <ilya.popov.sbmt@gmail.com>
2018-02-26 15:36:41 +0300

    partly done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let opened = [];
+    let closed = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let patternO = new RegExp('\\' + bracketsConfig[i][0], 'g');
+        opened += str.match(patternO);
+        let patternC = new RegExp('\\' + bracketsConfig[i][1], 'g');
+        closed += str.match(patternC);
+    }
+    
+    return opened.length === closed.length
+};
